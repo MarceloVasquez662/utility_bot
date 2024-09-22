00c83eb2ed340767a0475a569ce94526721e8c80
Marcelo Vasquez <58239732+MarceloVasquez662@users.noreply.github.com>
2024-09-22 19:42:51 +0000

    add description to smokes

--- src/executables/smoke.js
+++ src/executables/smoke.js
@@ -1,12 +1,14 @@
 import getSmokes from "../data/getSmokes.js";
 
 export default async function smokeExecutable(interaction, selectedMap, zone){
-        const smoke = await getSmokes()
-            .then(smoke => smoke[selectedMap][zone]);
+    const {smoke, description} = await getSmokes()
+    .then(smoke => smoke[selectedMap][zone]);
 
-        if(undefined === smoke) {
-            interaction.reply(`No seleccionaste un humo valido 🙉`);
-        } else {
-            interaction.reply(`Aqui va tu smoke de ${selectedMap} - ${zone} 😎 \n Imagen de referencia: ${smoke}`);
-        }
+    if(undefined === smoke) {
+        interaction.reply(`No seleccionaste un humo valido 🙉`);
+    } else {
+        interaction.reply(`Aqui va tu smoke de ${selectedMap} - ${zone} 😎 
+            \n Descripción: ${description}
+            \n Imagen de referencia: ${smoke}`);
+    }
 };
